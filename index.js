150b823d190ae85ea21487e75c44903e6ac3d35a
hera <a.heraimenka@gmail.com>
2020-07-06 13:45:04 -0700

    Create a user

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const shortid = require("shortid");
 
 const app = express();
+app.use(express.json());
+
 const PORT = 8000;
 const ADDRESS = "127.0.0.1";
 
@@ -19,6 +21,34 @@ let users = [
 app.get("/api/users", (req, res) => {
     res.json(users);
 });
+
+
+
+// Post a new user
+
+app.post("/api/users", (req, res) => {
+    let receivedData = req.body;
+
+    if (receivedData.name && receivedData.bio) {
+
+        let newUser = {
+            id: shortid.generate(),
+            name: receivedData.name,
+            bio: receivedData.bio
+        };
+
+        users.push(newUser);
+
+        res.status(201).json(newUser);
+
+    } else {
+        res.status(400).json({
+            error: "Please provide user name and bio"
+        });
+    }
+
+});
+
 
 
 app.listen(PORT, ADDRESS, () => {
