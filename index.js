c71e0eecbcdba2b5fc5878b0b7ef85be9f6b3ada
hera <a.heraimenka@gmail.com>
2020-07-06 13:13:07 -0700

    Get all users

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require("express");
+const shortid = require("shortid");
 
 const app = express();
 const PORT = 8000;
 const ADDRESS = "127.0.0.1";
+
+let users = [
+    {
+        id: shortid.generate(),
+        name: "Jane Doe",
+        bio: "Not Tarzan's Wife, another Jane",
+    }
+];
+
+
+// Get all users
+
+app.get("/api/users", (req, res) => {
+    res.json(users);
+});
 
 
 app.listen(PORT, ADDRESS, () => {
